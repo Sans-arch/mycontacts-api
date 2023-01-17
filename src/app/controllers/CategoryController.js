43789d22f38a1a41c5ec6912342f75e2612d02ab
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-17 08:27:39 -0300

    Add findById category method

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,3 +1,4 @@
+const idValidator = require('uuid-validate');
 const CategoryRepository = require('../repositories/CategoryRepository');
 
 class CategoryController {
@@ -5,6 +6,24 @@ class CategoryController {
     const categories = await CategoryRepository.findAll();
 
     return response.json(categories);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const isValidId = idValidator(id);
+
+    if (!isValidId) {
+      return response.status(400).json({ error: 'Invalid id' });
+    }
+
+    const category = await CategoryRepository.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    return response.json(category);
   }
 
   async store(request, response) {
